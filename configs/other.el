0441252fe51722793758d4e0a5827a6435116682
Kaushal Prajapati <kaushal.prajapati3@gmail.com>
2018-08-20 13:43:03 +0530

    initial commit

--- configs/other.el
+++
@@ -1,6 +0,0 @@
-(packages-conditional-install '(annoying-arrows-mode csv-mode markdown-mode encourage-mode which-key))
-
-(annoying-arrows-mode 1)
-(encourage-mode 1)
-(which-key-mode)
-
--- init.el
+++ init.el
@@ -1,27 +1,24 @@
-;; add package repositories
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
-(add-to-list 'package-archives '("marmalade" . "http://marmalade.ferrier.me.uk/packages/"))
-(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
-(add-to-list 'load-path "~/.emacs.d/zoom-frm.el")
+;;; init.el --- Configuration of Emacs
+
 (package-initialize)
 
-(when (not package-archive-contents)
-  (package-refresh-contents))
+(require 'org)
+(require 'ob-tangle)
 
-;; save when loosing focus
-;; (add-hook 'focus-out-hook 'save-some-buffers)
-
-;; order matter
-(load "~/.emacs.d/configs/install_first")
-(load "~/.emacs.d/configs/osx")
-(load "~/.emacs.d/configs/yasnippet")
-(load "~/.emacs.d/configs/scala")
-(load "~/.emacs.d/configs/misc")
-(load "~/.emacs.d/configs/ui")
-(load "~/.emacs.d/configs/editing")
-(load "~/.emacs.d/configs/project")
-(load "~/.emacs.d/configs/git")
-(load "~/.emacs.d/configs/windows")
-(load "~/.emacs.d/configs/other")
-(load "~/.emacs.d/configs/org")
+(setq init-dir (file-name-directory (or load-file-name (buffer-file-name
+))))
+(org-babel-load-file (expand-file-name "loader.org" init-dir))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (yasnippet use-package key-chord auto-package-update))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
