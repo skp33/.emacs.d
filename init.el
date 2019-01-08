9e1837ffc4048f06022ef4c5d718cf60831834dd
Kaushal Prajapati <Kaushal.Prajapati@go-mmt.com>
2019-01-08 22:47:35 +0530

    Updating new version of emacs configuration

--- init.el
+++ init.el
@@ -13,9 +13,20 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["black" "#d55e00" "#009e73" "#f8ec59" "#0072b2" "#cc79a7" "#56b4e9" "white"])
+ '(custom-enabled-themes (quote (arjen-grey)))
+ '(custom-safe-themes
+   (quote
+    ("1e9001d2f6ffb095eafd9514b4d5974b720b275143fbc89ea046495a99c940b0" default)))
+ '(hl-paren-colors
+   (quote
+    ("#B9F" "#B8D" "#B7B" "#B69" "#B57" "#B45" "#B33" "#B11")))
  '(package-selected-packages
    (quote
-    (yasnippet use-package key-chord auto-package-update))))
+    (transpose-frame treemacs magit yasnippet use-package key-chord auto-package-update))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
